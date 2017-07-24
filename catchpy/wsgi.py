b457949bce737850d5966956d0c651f2b828e025
nmaekawa <nmaekawa@g.harvard.edu>
2017-07-24 10:11:57 -0400

    fix func references; add dotenv j2 template

--- catchpy/wsgi.py
+++ catchpy/wsgi.py
@@ -13,8 +13,9 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # it is ironic that the path for env vars is set in an env var...
-dotenv_path = os.environ.get('CATCHPY_DOTENV_PATH',
-                             os.path.join(dirname(__file__), 'catchpy.env'))
+dotenv_path = os.environ.get(
+    'CATCHPY_DOTENV_PATH',
+    os.path.join(os.path.dirname(__file__), 'catchpy.env'))
 load_dotenv(dotenv_path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "catchpy.settings.dev")
